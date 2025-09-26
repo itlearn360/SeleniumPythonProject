685ac23fdb4e7142f2d40b08adcfe20bd05e3ab2
itlearn360 <40198951+itlearn360@users.noreply.github.com>
2025-09-26 17:11:29 +0530

    Update implicitWait.py

--- implicitWait.py
+++ implicitWait.py
@@ -14,11 +14,11 @@ driver = webdriver.Chrome(options=options)
 
 driver.get("https://demo.itlearn360.com/")
 driver.implicitly_wait(10)
-
-
+driver.manage().window().maximize();
 driver.find_element(By.ID,'loginlabel').click()
 driver.find_element(By.NAME,'log').send_keys("Demo12")
 driver.find_element(By.NAME,'pwd').send_keys("Test123456$")
 
 driver.find_element(By.NAME,'wp-submit').click()
+
 
